014af9b72344c01a571a97127acf1c7c040ccf55
Matheus Novais de Campos <mthscampos@gmail.com>
2021-06-02 22:57:07 -0300

    Versão 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <div className="container-form">
+      <div className="background-top" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="container-form-title">
+            <h2>Esqueceu a senha?</h2>
+            <p>
+              Por favor, digite seu e-mail abaixo. Nele, voce receberá uma senha
+              provisória.
+            </p>
+          </div>
+          <div className="container-form-input">
+           
+            <input
+              type="email"
+              name="input-email"
+              className="input-email-form"
+              placeholder="Digite seu e-mail"
+              {...register("email", { required: true })}
+            />
+            {errors.email?.type === "required" && (
+              <span className="msg-error">Campo obrigatório</span>
+            )}
+            <button className="btn-confirm">enviar</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
